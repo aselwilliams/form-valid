c12b4d1dd63b46672ab82dc8b7b7dd1eac55cca5
aselwilliams <aselwilliams22@gmail.com>
2022-05-25 15:52:16 -0400

    patterns,error msgs included

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import './FormInput.css'
+import './FormInput.css';
+import { useState } from 'react';
 
 export const FormInput = (props) => {
+    const [focused, setFocused]=useState(false)
     const {label,onChange,errorMessage, id,...inputProps}=props
+
+    const handleFocus=(e)=>{
+        setFocused(true)
+    }
   return (
     <div className='form-input'>
         <label>{label}: </label>
-        <input {...inputProps} onChange={onChange}/>
+        <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()} onFocus={()=>inputProps.name==='confirmPassword' && setFocused(true)} />
         <span>{errorMessage}</span>
     </div>
   )
