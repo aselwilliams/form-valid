ff05a6a11f3cf3927f6a866b2b52eb047d5f7abd
aselwilliams <aselwilliams22@gmail.com>
2022-05-25 12:15:59 -0400

    form setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState,useEffect,useRef} from 'react'
 import './App.css';
+import { FormInput } from './components/FormInput';
 
 function App() {
+  // const [username, setUsername] = useState('')
+  // const usernameRef=useRef()
+
+  console.log('re-render')
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+   const data=new FormData(e.target)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <FormInput name='username' placeholder='Username' />
+        <FormInput name='email' placeholder='Email' />
+        <FormInput name='full-name' placeholder='Full Name' />
+        <FormInput name='password' placeholder='Password' />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './FormInput.css'
+
+export const FormInput = (props) => {
+  return (
+    <div className='form-input'>
+        <label htmlFor="">{props.placeholder}: </label>
+        <input type="text" placeholder={props.placeholder} />
+    </div>
+  )
+}
